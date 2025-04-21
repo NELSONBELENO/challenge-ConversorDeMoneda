9c646e6723569ea9b5660ba73d6ea2628f906158
NELSON <jnbsthebest@gmail.com>
2025-04-21 09:22:59 -0500

    Primer Commit Conversor de Modena

---
+++ src/main/java/challenge/conversordemoneda/ChallengeConversorDeMonedaApplication.java
@@ -0,0 +1,13 @@
+package challenge.conversordemoneda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChallengeConversorDeMonedaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChallengeConversorDeMonedaApplication.class, args);
+    }
+
+}
